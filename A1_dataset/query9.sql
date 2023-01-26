a11e698e7d99130baa2a712c35970bc343f9712b
alexriderspy <sreemantidey1234@gmail.com>
2023-01-27 01:38:22 +0530

    10th done

--- A1_dataset/query9.sql
+++ A1_dataset/query9.sql
@@ -1,6 +1,7 @@
-
-
-select avg(t.salary) from (
+select player_category, tab2.avg_salary from (
+select max(avg_salary) as max_salary from (
+select 'batsman' player_category, p1.bat_avg avg_salary from (
+select avg(t1.salary) as bat_avg from (
 select
     distinct salaries.yearid,
     salary,
@@ -8,4 +9,40 @@ select
 from
     salaries
     join batting on salaries.playerid = batting.playerid    
-) as t;
+) as t1
+) as p1
+union 
+select 'pitcher' player_category, p2.pit_avg avg_salary from 
+(select avg(t2.salary) as pit_avg from (
+select
+    distinct salaries.yearid,
+    salary,
+    salaries.playerid
+from
+    salaries
+    join pitching on salaries.playerid = pitching.playerid    
+) as t2
+) as p2) as tab) as tab1 join (
+select 'batsman' player_category, p1.bat_avg avg_salary from (
+select avg(t1.salary) as bat_avg from (
+select
+    distinct salaries.yearid,
+    salary,
+    salaries.playerid
+from
+    salaries
+    join batting on salaries.playerid = batting.playerid    
+) as t1
+) as p1
+union 
+select 'pitcher' player_category, p2.pit_avg avg_salary from 
+(select avg(t2.salary) as pit_avg from (
+select
+    distinct salaries.yearid,
+    salary,
+    salaries.playerid
+from
+    salaries
+    join pitching on salaries.playerid = pitching.playerid    
+) as t2
+) as p2) as tab2 on tab2.avg_salary = tab1.max_salary;
--- A1_dataset/test.sql
+++
@@ -1,9 +0,0 @@
-select avg(t.salary) from (
-select
-    distinct salaries.yearid,
-    salary,
-    salaries.playerid
-from
-    salaries
-    join pitching on salaries.playerid = pitching.playerid    
-) as t;
