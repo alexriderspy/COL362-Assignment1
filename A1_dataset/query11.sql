f257371395b8c457dcb98a744375a762c78fdcc0
alexriderspy <sreemantidey1234@gmail.com>
2023-01-31 20:50:30 +0530

    aa

--- A1_dataset/query11.sql
+++ A1_dataset/query11.sql
@@ -1,43 +1,64 @@
+with a as (
+    select
+        teamid,
+        name
+    from
+        teams t1
+    where
+        yearid = (
+            select
+                max(yearid)
+            from
+                teams t2
+            where
+                t1.teamid = t2.teamid
+        )
+),
+b as (
+    select
+        teams.teamid,
+        count(wswin) as total_ws_wins
+    from
+        (
+            select
+                teamid,
+                yearid
+            from
+                (
+                    select
+                        teamid,
+                        yearid,
+                        sum(g) as s
+                    from
+                        teams
+                    where
+                        wswin = 'y'
+                    group by
+                        teamid,
+                        yearid
+                ) as tb
+            where
+                tb.s >= 110
+        ) as t
+        join teams on teams.teamid = t.teamid
+        and teams.yearid = t.yearid
+    where
+        wswin = 'y'
+    group by
+        teams.teamid
+)
 select
-    teams.teamid,
-    teams.name as teamname,
-    count(wswin) as total_ws_wins
+    b.teamid,
+    name as teamname,
+    total_ws_wins
 from
-    (
-        select
-            teamid,
-            name,
-            yearid
-        from
-            (
-                select
-                    teamid,
-                    name,
-                    yearid,
-                    sum(g) as s
-                from
-                    teams
-                where
-                    wswin = 'y'
-                group by
-                    teamid,
-                    name,
-                    yearid
-            ) as tb
-        where
-            tb.s >= 110
-    ) as t
-    join teams on teams.teamid = t.teamid
-    and teams.yearid = t.yearid
-    and teams.name = t.name
+    a,
+    b
 where
-    wswin = 'y'
-group by
-    teams.teamid,
-    teamname
+    a.teamid = b.teamid
 order by
     total_ws_wins desc,
-    teams.teamid asc,
+    b.teamid asc,
     teamname asc
 limit
     5;
